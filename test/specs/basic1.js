2f86e98b50991fe48d1366e83a4cf6f6cfa86295
urkazan16 <urkazan16@gmail.com>
2019-07-26 14:33:01 +0300

    asdsa

--- test/specs/basic1.js
+++ test/specs/basic1.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 
 describe('webdriver.io page', () => {
-
-    it('test 2', () => {
+    it('should have the right title', () => {
         browser.url('https://webdriver.io');
         const title = browser.getTitle();
-        assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node 2.js');
+        assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
+
 });
 
